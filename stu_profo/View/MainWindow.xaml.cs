c99def218570d5d969ca8e5c1c558b08613fc6d7
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-07-18 15:01:09 +0530

    Dynamically update

--- stu_profo/View/MainWindow.xaml.cs
+++ stu_profo/View/MainWindow.xaml.cs
@@ -1,40 +1,77 @@
-﻿using System.Text;
+﻿using System;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net.NetworkInformation;
-using static System.Net.WebRequestMethods;
-using System.Data;
 using stu_profo.Controller;
 
 namespace stu_profo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private bool status = false;  
+        private bool status = false;
+
+        private Brush _mainBackground;
+        public Brush MainBackground
+        {
+            get { return _mainBackground; }
+            set
+            {
+                _mainBackground = value;
+                OnPropertyChanged(nameof(MainBackground));
+            }
+        }
+
+        private ImageBrush _mainBackgroundImage;
+        public ImageBrush MainBackgroundImage
+        {
+            get { return _mainBackgroundImage; }
+            set
+            {
+                _mainBackgroundImage = value;
+                OnPropertyChanged(nameof(MainBackgroundImage));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            // Set default background
+            MainBackground = new RadialGradientBrush
+            {
+                GradientOrigin = new Point(0.5, 0.5),
+                Center = new Point(0.5, 0.5),
+                RadiusX = 0.5,
+                RadiusY = 0.5,
+                GradientStops = new GradientStopCollection
+                {
+                    new GradientStop((Color)ColorConverter.ConvertFromString("#1a4e96"), 0.0),
+                    new GradientStop((Color)ColorConverter.ConvertFromString("#164381"), 0.7),
+                    new GradientStop((Color)ColorConverter.ConvertFromString("#0C3771"), 1.0)
+                }
+            };
+
             loadingScreen.Visibility = Visibility.Visible;
-            //validateLoad();
-
-
         }
-        public Boolean validateLoad() {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool ValidateLoad()
+        {
             try
             {
-
                 Ping myPing = new Ping();
-                String host = "www.nibmworldwide.com";
+                string host = "www.nibmworldwide.com";
                 byte[] buffer = new byte[32];
                 int timeout = 1000;
                 PingOptions pingOptions = new PingOptions();
@@ -44,11 +81,11 @@ namespace stu_profo
             }
             catch (Exception)
             {
-                
                 status = true;
                 return false;
             }
         }
+
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -63,6 +100,7 @@ namespace stu_profo
         {
             // Trigger binding update
         }
+
         private async void cont(object sender, RoutedEventArgs e)
         {
             statusLabel.Content = "Checking....";
@@ -71,11 +109,9 @@ namespace stu_profo
             await Task.Delay(1000);
 
             statusLabel.Content = "Checking dependencies";
-            if (validateLoad())
+            if (ValidateLoad())
             {
                 statusLabel.Content = "Checking done";
-            
-
                 System.Diagnostics.Debug.WriteLine("Connection success!");
 
                 await Task.Delay(3000);
@@ -85,7 +121,7 @@ namespace stu_profo
             }
             else
             {
-                statusLabel.Content = "Checking faild! Restart the application....";
+                statusLabel.Content = "Checking failed! Restart the application....";
                 signupScreen.Visibility = Visibility.Hidden;
                 loadingScreen.Visibility = Visibility.Visible;
                 System.Diagnostics.Debug.WriteLine("Connection failed!");
@@ -98,24 +134,45 @@ namespace stu_profo
             System.Diagnostics.Debug.WriteLine(emailInput.Text + passwordInput.Text);
             if (userCtn.validateUser(emailInput.Text, passwordInput.Text))
             {
-                //blurScreen.Visibility = Visibility.Hidden;
                 signupScreen.Visibility = Visibility.Hidden;
                 desktop4.Visibility = Visibility.Visible;
+
+                // Set background for desktop4
+                MainBackground = Brushes.White;
+                MainBackgroundImage = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/View/Group 1000001063.png")));
             }
             else
             {
                 signupScreen.Visibility = Visibility.Visible;
                 desktop3.Visibility = Visibility.Hidden;
-                //blurScreen.Visibility = Visibility.Visible;
+
+                // Set background for signup screen
+                MainBackground = new RadialGradientBrush
+                {
+                    GradientOrigin = new Point(0.5, 0.5),
+                    Center = new Point(0.5, 0.5),
+                    RadiusX = 0.5,
+                    RadiusY = 0.5,
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop((Color)ColorConverter.ConvertFromString("#1a4e96"), 0.0),
+                        new GradientStop((Color)ColorConverter.ConvertFromString("#164381"), 0.7),
+                        new GradientStop((Color)ColorConverter.ConvertFromString("#0C3771"), 1.0)
+                    }
+                };
+                MainBackgroundImage = null;
             }
             System.Diagnostics.Debug.WriteLine("calling");
-
         }
 
         private void showinfo_Click(object sender, RoutedEventArgs e)
         {
             desktop4.Visibility = Visibility.Hidden;
             desktop3.Visibility = Visibility.Visible;
+
+            // Set background for desktop3
+            MainBackground = Brushes.White;
+            MainBackgroundImage = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/View/Group 1000001063.png")));
         }
     }
 }
