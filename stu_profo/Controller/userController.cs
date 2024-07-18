594ba0b2c2de1be53cc9762c90419af57e6e170f
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-07-18 08:05:17 +0530

    fixes

--- stu_profo/Controller/userController.cs
+++ stu_profo/Controller/userController.cs
@@ -4,18 +4,38 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.IO;
+    using System.Text.Json;
+using System.Data;
 
-    namespace stu_profo.Controller
+namespace stu_profo.Controller
+{
+    class userController
     {
-        class userController
+        public static void validateUser()
         {
-            public static void validateUser()
-            {
-                engine en = new engine();
-                en.dumpProgrammes();
-                //var dataset = en.getDump("https://www.nibmworldwide.com/", "exams/mis");
-                //System.Diagnostics.Debug.WriteLine(dataset.ToString());
-        }
-        }
+            //engine en = new engine();
+            //en.dumpProgrammes();
+            ////var dataset = en.getDump("https://www.nibmworldwide.com/", "exams/mis");
+            ////System.Diagnostics.Debug.WriteLine(dataset.ToString());
+            ///
 
+            string userData = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "users.json"));
+            var userBin = JsonSerializer.Deserialize<Users>(userData);
+            foreach (var user in userBin.users)
+            {
+                System.Diagnostics.Debug.WriteLine(user.email);
+            }
+        }
+        public class User
+        {
+            public string email { get; set; }
+            public string pwd { get; set; }
+
+        }
+        public class Users
+        {
+            public List<User> users { get; set; }
+        }
     }
+}
