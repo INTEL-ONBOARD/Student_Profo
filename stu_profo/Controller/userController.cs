7b6c07e365bc7942025b93b9b1b107467091c122
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-07-30 17:40:50 +0530

    2.0 : array for user auth added!

--- stu_profo/Controller/userController.cs
+++ stu_profo/Controller/userController.cs
@@ -7,6 +7,8 @@
     using System.IO;
     using System.Text.Json;
 using System.Data;
+using System.Configuration;
+
 
 namespace stu_profo.Controller
 {
@@ -22,11 +24,21 @@ namespace stu_profo.Controller
 
             string userData = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "users.json"));
             var userBin = JsonSerializer.Deserialize<Users>(userData);
+            int sizeOfUsers = userBin.users.Count();
+            customArray userArray = new customArray(sizeOfUsers);
             foreach (var user in userBin.users)
             {
-                if (user.email == useremail && user.password == pwd) { return true; } else { return false; }
+               userArray.insert(user);
             }
-            return false;
+            if ((userArray.search("email", useremail)) && (userArray.search("password", pwd)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
         public class User
         {
@@ -37,6 +49,47 @@ namespace stu_profo.Controller
         public class Users
         {
             public List<User> users { get; set; }
+        }
+        
+        public class customArray
+        {
+            User[] userSet;
+            int arrayBin;
+            public customArray(int length)
+            {
+                userSet = new User[length];
+                arrayBin = 0;
+            }
+           public void insert(User user)
+            {
+                userSet[arrayBin] = user;
+                arrayBin++;
+            }
+            public void remove(User user, int pos) {
+                userSet[pos] = null;
+            }
+            public void clear() {
+                userSet = null;
+            }
+            public bool search(string types,string pattern) {
+                string type = types;
+                if (type == "email")
+                {
+                    foreach (var user in userSet)
+                    {
+                        if (user.email == pattern) { return true; }
+                    }
+                }
+                else if (type == "password")
+                {
+                    foreach (var user in userSet)
+                    {
+                        if(user.password == pattern) {return true; }
+                    }
+                }
+
+                return false;
+            }
         }
     }
 }
